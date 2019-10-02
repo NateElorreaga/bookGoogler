e85073d4e99bb6c55590bb94c2f471f258dd028c
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-10-01 20:47:07 -0600

    adding components

---
+++ my-app/client/src/components/Nabar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function Navbar(props) {
+    return (
+        <div className="navbar sticky-top navbar-light bg-light">
+            Book Googler
+        </div>
+    );
+}
